e7939b5c87a7aebc6ce125fd561b5001d27c5d63
joudyjrrr <joudyjrrr@gmail.com>
2023-11-30 17:44:06 +0300

    ..

--- src/API/axios.ts
+++ src/API/axios.ts
@@ -1,3 +1,5 @@
+import { newAbortSignal } from "../helper/DateHelpers";
+import { showError } from "../libs/reactToastify";
 import { API_BASE_URL } from "./domain";
 import axios from "axios";
 
@@ -9,6 +11,7 @@ DeliveryApiInstances.interceptors.request.use((config) => {
   if (token) config.headers.setAuthorization(`Bearer ${token}`);
   return config;
 });
+let timerToAbort = 5000;
 DeliveryApiInstances.interceptors.response.use(
   (response) => response,
   (error) => {
@@ -16,6 +19,25 @@ DeliveryApiInstances.interceptors.response.use(
       localStorage.clear();
       window.location.reload();
     } else throw error.response.data.errorMessage;
+  }
+);
+DeliveryApiInstances.interceptors.response.use(
+  (response) => {
+    timerToAbort = 5000
+    return response},
+  async (error) => {
+    if (error.response.status === 401) {
+      const signal = newAbortSignal(timerToAbort);
+      timerToAbort -= timerToAbort;
+      localStorage.clear();
+      window.location.reload();
+      console.log(signal)
+    
+    } else {
+      showError(error.response.data.errorMessage);
+      const _error = new Error(error.response.data.errorMessage);
+      throw _error;
+    }
   }
 );
 export default DeliveryApiInstances;
